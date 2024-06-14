a6b1cead00370d388a2397e52a40edf4125f64e4
Leonardo Rojas Q <leonardorojasq@outlook.com>
2024-06-14 06:53:23 -0500

    Se cambio el nombre al curso

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hola Mundo
+         Curso Intensivo
         </p>
         <a
           className="App-link"
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Curso de reactjs
         </a>
       </header>
     </div>
